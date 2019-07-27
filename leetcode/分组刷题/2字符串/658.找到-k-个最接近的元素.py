5fa845667a71652d62f466306dcdab15d9900fde
FishRedLeaf <37491656+FishRedLeaf@users.noreply.github.com>
2019-07-27 16:56:19 +0800

    Delete 658.找到-k-个最接近的元素.py

--- leetcode/分组刷题/2字符串/658.找到-k-个最接近的元素.py
+++
@@ -1,9 +0,0 @@
-#
-# @lc app=leetcode.cn id=658 lang=python3
-#
-# [658] 找到 K 个最接近的元素
-#
-class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        
-
