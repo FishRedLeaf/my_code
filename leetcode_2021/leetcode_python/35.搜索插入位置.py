4327982c0e2f9e1abc83a0cbccb5f592f5526f55
yanbingjian <18795886275@163.com>
2021-03-01 22:52:40 +0800

    add:array_0301

---
+++ leetcode_2021/leetcode_python/35.搜索插入位置.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return 0
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + ((right - left) >> 1)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
